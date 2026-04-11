c10ddf6085300e4698ad0356b3568c5511cdaeee
spano14 <67838888+spano14@users.noreply.github.com>
2026-04-11 16:24:40 +0200

    fix

--- resources/views/partials/site-header.blade.php
+++ resources/views/partials/site-header.blade.php
@@ -41,16 +41,35 @@
         </nav>
         <div class="site-header__actions">
             <div class="site-header__cta">
-                <a class="site-header__quote" href="{{ $quoteHref }}">
-                    <span class="site-header__quote-text">Get a Quote</span>
-                    <span class="site-header__quote-icon" aria-hidden="true">
-                        <img
-                            src="{{ asset('assets/images/af40b499a662ffc7-3NSshCmK7vz6VHCPAq6twmsYQ.svg') }}"
-                            width="40"
-                            height="40"
-                            alt=""
-                        />
-                    </span>
+                <a
+                    class="site-header__quote framer-1sg2rgo framer-164f1w4"
+                    data-framer-name="wrapper"
+                    href="{{ $quoteHref }}"
+                >
+                    <div class="framer-1ohlfe3" data-framer-name="text">
+                        <div class="framer-kfq9mg">
+                            <p class="framer-text framer-styles-preset-md7q5c site-header__quote-text" dir="auto">
+                                Get a Quote
+                            </p>
+                        </div>
+                    </div>
+                    <div class="framer-uhu0pr" data-framer-name="icon">
+                        <div class="framer-qcft76" data-framer-name="icon">
+                            <div class="framer-4qeivn">
+                                <div data-framer-background-image-wrapper="true">
+                                    <img
+                                        class="site-header__quote-img"
+                                        src="{{ asset('assets/images/af40b499a662ffc7-3NSshCmK7vz6VHCPAq6twmsYQ.svg') }}"
+                                        width="40"
+                                        height="40"
+                                        alt=""
+                                        decoding="auto"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="framer-9gjsjv" data-framer-name="bg" aria-hidden="true"></div>
+                    </div>
                 </a>
             </div>
             <button
