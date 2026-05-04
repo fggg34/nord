fd54dc56dd78b3501792bcd1773414d690d9ef6c
Kevin Hitaj <hitajkevin10@gmail.com>
2026-05-04 12:44:12 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html{!! $htmlAttrs ?? ' lang="en" class="lenis"' !!}>
 <head>
+    @php
+        $__framerOverridesPath = public_path('css/framer-overrides.css');
+        $__siteHeaderCssPath = public_path('css/site-header.css');
+        $__framerOverridesV = is_file($__framerOverridesPath) ? filemtime($__framerOverridesPath) : 0;
+        $__siteHeaderCssV = is_file($__siteHeaderCssPath) ? filemtime($__siteHeaderCssPath) : 0;
+    @endphp
     <base href="{{ url('/') }}">
     <meta name="google-site-verification" content="d6FRz3-dsK2ETo5_XnY6biS-YI6XTHydGzY8J3kanvE">
     @yield('head_inner')
-    <link rel="stylesheet" href="{{ asset('css/framer-overrides.css') }}?v={{ is_file($__p = public_path('css/framer-overrides.css')) ? filemtime($__p) : '0' }}">
-    <link rel="stylesheet" href="{{ asset('css/site-header.css') }}?v={{ is_file($__p = public_path('css/site-header.css')) ? filemtime($__p) : '0' }}">
     @stack('styles')
     @include('partials.site-branding-head')
+    {{-- After Framer head + @stack: overrides win; ?v = filemtime for cache bust --}}
+    <link rel="stylesheet" href="{{ asset('css/framer-overrides.css') }}?v={{ $__framerOverridesV }}">
+    <link rel="stylesheet" href="{{ asset('css/site-header.css') }}?v={{ $__siteHeaderCssV }}">
 </head>
 <body class="{{ request()->is('/') ? 'page-home' : 'page-inner' }}{{ request()->is('contact-us') ? ' page-contact-us' : '' }}">
     <script src="{{ asset('js/cms-framer-split-titles.js') }}"></script>
